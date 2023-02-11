fc535f4b3f881bde0804863bfd9617d2019af619
Cawe541 <cawe.wewe04@gmail.com>
2023-02-11 18:32:53 -0300

    add calculadora de descontos

---
+++ src/CalculadoraDeDescontos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class CalculadoraDeDescontos
+{
+    public function calculaDescontos(Orcamento $orcamento): float
+    {
+        return $orcamento->quantidadeDeItems > 5 ? $orcamento->valor * 0.1 : $orcamento->valor * 1;
+    }
+}
