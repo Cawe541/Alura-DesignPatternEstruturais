8faa2e327a96e083065db8a37e053486294af67e
Cawe541 <cawe.wewe04@gmail.com>
2023-02-19 12:36:43 -0300

    create class RegistroOrcamento

---
+++ src/RegistroOrcamento.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class RegistroOrcamento
+{
+    public function registrar(Orcamento $orcamento): void
+    {
+        // chamar uma api de registro
+        // enviar os dados do orcamento
+    }
+}
