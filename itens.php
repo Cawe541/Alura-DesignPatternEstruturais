e31ec5611daa4383ec003fe7cc88936a035eb4a9
Cawe541 <cawe.wewe04@gmail.com>
2023-02-21 13:03:53 -0300

    implement Proxy of Cache

--- itens.php
+++ itens.php
@@ -2,7 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-use Alura\DesignPattern\{Orcamento, ItemOrcamento};
+use Alura\DesignPattern\{CacheOrcamentoProxy, Orcamento, ItemOrcamento};
 
 $orcamento = new Orcamento();
 $orcamento->adicionaItem(new ItemOrcamento(500));
@@ -13,5 +13,34 @@ $orcamentoMaisAntigoAinda->adicionaItem(new ItemOrcamento(200));
 $orcamentoAntigo->adicionaItem($orcamentoMaisAntigoAinda);
 $orcamento->adicionaItem($orcamentoAntigo);
 
-echo $orcamento->valor() . PHP_EOL;
-echo $orcamento->valor() . PHP_EOL;
+$proxyCache = new CacheOrcamentoProxy($orcamento);
+
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
+echo $proxyCache->valor() . PHP_EOL;
